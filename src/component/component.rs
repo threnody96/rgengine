1c6cf5b234dbf41fdae5ab6d262e37b245151b7c
Imota Eru <threnody96@gmail.com>
2019-07-28 19:46:05 +0900

    master

--- src/component/component.rs
+++ src/component/component.rs
@@ -4,9 +4,11 @@ use ::resource::Resource;
 
 pub trait Component {
     
-    fn update(&mut self, ctx: &mut Context, rsc: Rc<Resource>) -> GameResult;
-
     fn draw(&self, ctx: &mut Context, rsc: Rc<Resource>) -> GameResult;
+
+    fn update(&mut self, _ctx: &mut Context, _rsc: Rc<Resource>) -> GameResult {
+        Ok(())
+    }
 
     fn detach(&self, _ctx: &mut Context, _rsc: Rc<Resource>) -> GameResult {
         Ok(())
