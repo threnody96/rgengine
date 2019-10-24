4c8388342b8b3d7db1e3c91d0fd3cacfdd3342aa
Imota Eru <threnody96@gmail.com>
2019-10-24 20:33:43 +0900

    master

--- src/application/game.rs
+++ src/application/game.rs
@@ -1,13 +1,15 @@
 use std::rc::Rc;
 use ::application::{ AppDelegate };
 use ::node::{ SceneLike };
-use ::util::{ Point, Size, director, context };
+use ::util::{ Point, Size, must, director, context };
 use ggez::{ Context, GameResult };
 use ggez::event::{ EventHandler };
 use ggez::timer::{ check_update_time, fps };
-use ggez::graphics::{ DrawParam, Text, BLACK, WHITE, draw, clear, present };
+use ggez::graphics::{ Canvas, DrawParam, Text, TextFragment, BLACK, WHITE, Font, Scale, draw, clear, present, set_canvas };
+use ggez::conf::NumSamples;
 
 pub struct Game {
+    canvas: Canvas,
     delegate: Rc<dyn AppDelegate>
 }
 
@@ -15,6 +17,7 @@ impl Game {
 
     pub fn new(delegate: Rc<dyn AppDelegate>) -> Self {
         Self {
+            canvas: context(|ctx| must(Canvas::new(ctx, 400, 300, NumSamples::One))),
             delegate: delegate
         }
     }
@@ -26,7 +29,13 @@ impl Game {
     fn draw_debug_message(&self) -> GameResult<()> {
         if director(|d| !d.get_display_stats()) { return Ok(()); }
         context(|ctx| {
-            let fps_display = Text::new(format!("FPS: {}", fps(ctx).round() as u32));
+            let fps_display = Text::new(TextFragment {
+                text: format!("FPS: {}", fps(ctx).round() as u32),
+                color: Some(WHITE),
+                font: Some(Font::default()),
+                scale: Some(Scale::uniform(15.0)),
+                ..Default::default()
+            });
             draw(
                 ctx,
                 &fps_display,
@@ -48,10 +57,22 @@ impl EventHandler for Game {
     }
 
     fn draw(&mut self) -> GameResult<()> {
-        context(|ctx| clear(ctx, BLACK));
+        context(|ctx| {
+            set_canvas(ctx, Some(&self.canvas));
+            clear(ctx, BLACK);
+        });
         self.get_scene().render();
         self.draw_debug_message();
-        context(|ctx| present(ctx))
+        context(|ctx| {
+            present(ctx);
+            set_canvas(ctx, None);
+            clear(ctx, BLACK);
+            ggez::graphics::draw(ctx, &self.canvas, DrawParam {
+                dest: Point { x: 0.0, y: 0.0 },
+                ..Default::default()
+            });
+            present(ctx)
+        })
     }
 
     fn resize_event(&mut self, width: f32, height: f32) {
